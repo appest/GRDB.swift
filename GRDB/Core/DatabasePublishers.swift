c95b508200d35f347509575c92f5fb4a895b8825
Ned Chan <chenchao21@gmail.com>
2021-11-04 19:03:39 +0800

    fix(combine): remove combine

--- GRDB/Core/DatabasePublishers.swift
+++ GRDB/Core/DatabasePublishers.swift
@@ -1,5 +1,5 @@
-#if canImport(Combine)
-/// A namespace for database Combine publishers.
-@available(OSX 10.15, iOS 13, tvOS 13, watchOS 6, *)
-public enum DatabasePublishers { }
-#endif
+//#if canImport(Combine)
+///// A namespace for database Combine publishers.
+//@available(OSX 10.15, iOS 13, tvOS 13, watchOS 6, *)
+//public enum DatabasePublishers { }
+//#endif
